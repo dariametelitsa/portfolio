f18917ab3e81d431fa6051a0cf72c81cc759d5e0
dariametelitsa <dariametelitsa@gmail.com>
2024-03-15 15:40:51 +0300

    Deleted unused logo

--- src/pages/contacts/Contacts.tsx
+++ src/pages/contacts/Contacts.tsx
@@ -4,8 +4,6 @@ import { FlexWrapper } from "../../components/FlexWrapper";
 import { ContactsList } from "../../components/contactsList/ContactsList";
 import { Container } from "../../components/Container";
 import styled from "styled-components";
-import dots from './../../assets/images/decoration/Dots.svg';
-import { mainTheme } from "../../components/styles/Theme";
 
 export const Contacts = () => {
     return (
@@ -14,35 +12,13 @@ export const Contacts = () => {
             <Container>
                 <FlexWrapper as={'section'}>
                     <Information>I’m interested in freelance opportunities. However, if you have other request or
-                        question, don’t
-                        hesitate to contact me</Information>
+                        question, don’t hesitate to contact me</Information>
                     <ContactsList/>
                 </FlexWrapper>
             </Container>
         </main>
     );
 };
-
-const StyledContacts = styled.section`
-    position: relative;
-
-    &::before {
-        content: url(${dots});
-        position: absolute;
-        left: -50px;
-        top: 150px;
-    }
-
-    &::after {
-        content: '';
-        position: absolute;
-        right: -50px;
-        top: 150px;
-        width: 155px;
-        height: 155px;
-        border: 1px solid ${mainTheme.colors.grey.light};
-    }
-`
 
 const Information = styled.p`
     width: 45%;
