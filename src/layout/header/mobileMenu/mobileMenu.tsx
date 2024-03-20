251045e1fce63cf2dec204e621f2e2537a19a61d
dariametelitsa <dariametelitsa@gmail.com>
2024-03-20 21:58:06 +0300

    closeBtn functional behavior was added

--- src/layout/header/mobileMenu/mobileMenu.tsx
+++ src/layout/header/mobileMenu/mobileMenu.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Menu } from "../../../components/menu/Menu";
 import { S } from "../Header_Styles";
 import { Button } from "../../../components/Button";
 import { MenuData } from "../Header";
 
 const MobileMenu: React.FC<{ items: Array<MenuData> }> = (props: { items: Array<MenuData> }) => {
+    const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+    function onBurgerClickBtn() {
+        setMenuIsOpen(!menuIsOpen);
+    }
+
     return (
         <S.MobileMenu>
-            <S.BurgerButton isOpen={false}>
+            <S.BurgerButton isOpen={menuIsOpen} onClick={onBurgerClickBtn}>
                 <span></span>
             </S.BurgerButton>
-            <S.MobileMenuPopup isOpen={false}>
+            <S.MobileMenuPopup isOpen={menuIsOpen} onClick={() => setMenuIsOpen(false)}>
                 <Menu items={props.items}/>
                 <S.Select name='language'>
                     <option value="en">En</option>
