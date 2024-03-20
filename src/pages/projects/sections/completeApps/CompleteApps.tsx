d11d0cb297bb76db963f70f306d06e887f0e2710
dariametelitsa <dariametelitsa@gmail.com>
2024-03-20 22:01:19 +0300

    tabs behavior was added

--- src/pages/projects/sections/completeApps/CompleteApps.tsx
+++ src/pages/projects/sections/completeApps/CompleteApps.tsx
@@ -1,20 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from "styled-components";
 import { Container } from "../../../../components/Container";
 import { SectionTitle } from "../../../../components/SectionTitle";
 import { ProjectsGallery } from "../../../../components/projectsGallery/ProjectsGallery";
-import { projectsBig } from "../../../../assets/Data";
+import { projectsBig, tabsItems } from "../../../../assets/Data";
+import { TabsMenu, TabsStatusType } from "../../../../components/tabsMenu/TabsMenu";
+import { ProjectCardPropsType } from "../../../../components/projectsGallery/projectCard/ProjectCard";
 
-export const CompleteApps = () => {
+
+function filterWorks(works: Array<ProjectCardPropsType>, currentFilterStatus: string) {
+
+    if (currentFilterStatus === 'all') {
+        return works;
+    }
+    let filteredWorks;
+
+    filteredWorks = works.filter((work) => work.tags.find(el => el === currentFilterStatus));
+
+    return filteredWorks;
+}
+
+
+export const CompleteApps: React.FC = () => {
+    const [currentFilterStatus, setCurrentFilterStatus] = useState('all');
+
+    function changeFilterStatus(value: TabsStatusType) {
+        setCurrentFilterStatus(value);
+    }
+
     return (
         <StyledCompleteApps>
             <Container>
                 <SectionTitle>complete-apps</SectionTitle>
-                <ProjectsGallery projects={projectsBig}/>
+                <TabsMenu tabsItems={tabsItems}
+                          changeFilterStatus={changeFilterStatus}
+                          currentFilterStatus={currentFilterStatus}/>
+                <ProjectsGallery projects={filterWorks(projectsBig, currentFilterStatus)}/>
             </Container>
         </StyledCompleteApps>
     );
 };
+
 
 const StyledCompleteApps = styled.section `
 `
